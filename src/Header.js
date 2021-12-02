0b534c37df18f64c0b402d85236b18021823cb95
Rai Menezes <Raimenezes85@gmail.com>
2021-12-02 19:42:25 -0300

    Criação do header

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+
+
+function Header(){
+    return(
+        <header className="header">
+            <h3>uma seleção de produtos</h3>
+            <h1>especial para você</h1>
+            <h5>Todos os produtos desta lista foram selecionados a partir de sua navegação. Aproveite</h5>
+            <button>Conheça a Linix</button>
+            <button>Ajude o algoritomo</button>
+            <button>Seus produtos</button>
+            <button>Compartilhe</button>
+        </header>
+    )
+}
+export default Header;
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import {Component} from "react";
+import Header from "./Header";
+import "./Home.css"
+
+
+export default class Home extends Component{
+    render() {
+      return(
+         <div class="estrutura">
+             <Header/>
+         </div>
+      )
+    }
+}
