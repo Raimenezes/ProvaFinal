f5ac6f044a53e4f3a6fbbf05f8f4dc4c2d67de4c
Rai Menezes <Raimenezes85@gmail.com>
2021-12-03 21:45:16 -0300

    versão final do back

---
+++ Spring/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO product(id, name, image, old_price, price, description)
+VALUES (0, 'Numero Produto 0', 'https://http2.mlstatic.com/D_NQ_NP_758236-MLA47936348200_102021-O.webp', 2.399, 1.399, 'Fotografia profissional no seu bolso
+Descubra infinitas possibilidades para suas fotos com as 4 câmeras principais de sua equipe. Teste sua criatividade e jogue com iluminação, diferentes planos e efeitos para obter ótimos resultados.');
+INSERT INTO product(id, name, image, old_price, price, description)
+VALUES (1, 'Numero Produto 1', 'https://http2.mlstatic.com/D_NQ_NP_811442-MLA48157804490_112021-O.webp', 2.699, 1.899, 'Fotografia profissional no seu bolso
+Descubra infinitas possibilidades para suas fotos com as 3 câmeras principais de sua equipe. Teste sua criatividade e jogue com iluminação, diferentes planos e efeitos para obter ótimos resultados.');
+INSERT INTO product(id, name, image, old_price, price, description)
+VALUES (2, 'Numero Produto 2', 'https://http2.mlstatic.com/D_NQ_NP_609356-MLB45721314144_042021-O.webp', 409, 404, '• GABINETE C/ 3 FANS - 3 FANS RGB FRONTAL GERENCIÁVEL C/ CONTROLADORA E CONTROLE REMOTO
+• A BAIA DE HD/SSD SUPORTA 1 HD DE 3.5" E 1 HD/SSD DE 2.5".
+• OS 3 FANS FRONTAIS JÁ VEM INSTALADO, BASTA APENAS CONECTAR NA CONTROLADORA APÓS ACOMODAR ELA NA INSTALAÇÃO.');
+INSERT INTO product(id, name, image, old_price, price, description)
+VALUES (3, 'Numero Produto 3', 'https://http2.mlstatic.com/D_NQ_NP_877269-MLA32170094259_092019-O.webp', 275, 235, 'HyperX® FURY DDR4 oferece um poderoso impulso para jogos, edição de vídeo e renderização com velocidades de até 3733MHz. Esta atualização de baixo custo está disponível em velocidades de 2400MHz-3733MHz, latências CL15-19, capacidades de módulo único de 4GB-32GB e capacidades de kit 16GB-128GB.');
